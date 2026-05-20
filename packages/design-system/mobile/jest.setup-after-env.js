332825143d0c740ff93dd77e1464c14ee7f337c8
Carlos Amorim <kaduvuotto@gmail.com>
2026-05-20 13:33:40 -0300

    chore(ds-mobile): remove duplicate renderer mock from jest setup

--- packages/design-system/mobile/jest.setup-after-env.js
+++ packages/design-system/mobile/jest.setup-after-env.js
@@ -14,18 +14,3 @@ jest.mock('react-native/Libraries/Renderer/shims/ReactNative', () => ({
     isChildPublicInstance: jest.fn(() => false),
   },
 }));
-
-// Also mock RendererProxy to avoid any indirect loads.
-jest.mock('react-native/Libraries/ReactNative/RendererProxy', () => ({
-  findNodeHandle: jest.fn(() => -1),
-  findHostInstance_DEPRECATED: jest.fn(() => null),
-  dispatchCommand: jest.fn(),
-  sendAccessibilityEvent: jest.fn(),
-  getNodeFromInternalInstanceHandle: jest.fn(() => null),
-  getPublicInstanceFromInternalInstanceHandle: jest.fn(() => null),
-  getPublicInstanceFromRootTag: jest.fn(() => null),
-  isChildPublicInstance: jest.fn(() => false),
-  renderElement: jest.fn(),
-  unmountComponentAtNodeAndRemoveContainer: jest.fn(),
-  unstable_batchedUpdates: jest.fn((fn) => fn()),
-}));
